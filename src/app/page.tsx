98b6b95ba957b3cc96da79343b6d5e5659f828c4
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-02-01 11:56:00 +0200

    added addCompany

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import AddCompanyButton from './components/components/add-company-button';
 import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page { new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active Label</StatusLabel>
       <StatusLabel status={Status.NotActive}>Active Label</StatusLabel>
       <StatusLabel status={Status.Pending}>Active Label</StatusLabel>
       <StatusLabel status={Status.Suspendet}>Active Label</StatusLabel>
+      <AddCompanyButton/>
     </main>
   );
 }
