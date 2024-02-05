8c960f221af1a726dd451963eacf71a552bb71ae
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-02-05 20:09:55 +0200

    added intercepting routers

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,21 +1,43 @@
-import React from "react";
-import Header from "../../components/header";
-import Toolbar from "../../components/toolbar";
-import SearchInput from "../../components/search-input";
-import AddCompanyButton from "../../components/add-company-button";
-import CompanyTable from "../../components/company-table";
-import CompanyRow from "../../components/company-row";
-import { Status } from "../../components/status-label";
+// import React from "react";
+// import Header from "../../components/header";
+// import Toolbar from "../../components/toolbar";
+// import SearchInput from "../../components/search-input";
+// import AddCompanyButton from "../../components/add-company-button";
+// import CompanyTable from "../../components/company-table";
+// import CompanyRow from "../../components/company-row";
+// import { Status } from "../../components/status-label";
 
-export interface PageProps{ }
-export default function Page({ }: PageProps) {
-    return (<>
-        <Header>Companies</Header>
-        <Toolbar action={<AddCompanyButton/>}>
-            <SearchInput/>
-        </Toolbar>
-        <CompanyTable>
-            <CompanyRow id={1} category={"Products"} company={"Costco"} status={Status.Pending} promotion={true} country={"USA"} joinedDate={"02.09.2023"}/>
-        </CompanyTable>
-    </>)
+// export interface PageProps{ }
+// export default function Page({ }: PageProps) {
+//     return (<>
+//         <Header>Companies</Header>
+//         <Toolbar action={<AddCompanyButton/>}>
+//             <SearchInput/>
+//         </Toolbar>
+//         <CompanyTable>
+//             <CompanyRow id={1} category={"Products"} company={"Costco"} status={Status.Pending} promotion={true} country={"USA"} joinedDate={"02.09.2023"}/>
+//         </CompanyTable>
+//     </>)
+// }
+import React from 'react';
+import CompanyTable from '@/app/components/company-table';
+import CompanyRow from '@/app/components/company-row';
+import { Status } from '@/app/components/status-label';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <CompanyTable>
+      <CompanyRow
+        id={1}
+        category="Products"
+        company="Costco"
+        status={Status.Pending}
+        promotion={true}
+        country="USA"
+        joinedDate="02.19.2023"
+      />
+    </CompanyTable>
+  );
 }
--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-import React, { useState } from "react";
-import Button from "./button";
-import dynamic from "next/dynamic";
-const CompanyFormModal = dynamic(()=>import("./company-form-modal"),{ssr:false})
+import React from "react";
+import Button from '@/app/components/button';
+// import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+// const CompanyFormModal = dynamic(()=>import("./company-form-modal"),{ssr:false})
 
 export default function AddCompanyButton() {
 
-    const [show, setShow] = useState(false)
+    // const [show, setShow] = useState(false)
+    const router = useRouter();
     return (<>
-        <Button onClick={() => setShow(true)}>Add company</Button>
-        <CompanyFormModal onSubmit={console.log} show={show} onClose={()=>setShow(false)}/>
+        <Button onClick={() => router.push('/companies/new')}>Add company</Button>
+        {/* <Button onClick={() => setShow(true)}>Add company</Button> */}
+        {/* <CompanyFormModal onSubmit={console.log} show={show} onClose={()=>setShow(false)}/> */}
     </>)
 }
